11f8328cb73455e6810c059b8552c46377c726d4
yenilikci <m.melihcelik017@outlook.com>
2021-09-02 20:53:33 +0300

    Add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <b-navbar toggleable="lg" type="light" variant="light">
+        <b-navbar-brand href="#" class="">Akvaryum UI</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Anasayfa</b-nav-item>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                Üyelik İşlemleri
+              </template>
+              <b-dropdown-item href="#">Giriş Yap</b-dropdown-item>
+              <b-dropdown-item href="#">Kayıt Ol</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped></style>
